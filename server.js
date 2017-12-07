18fd253380a429f4b3fd5920e249c935e0b743c9
David Farrenkopf <dfarrenk@gmail.com>
2017-12-07 11:55:38 -0500

    Two servers, one file

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 var http = require("http");
 var PORT = 7000; //C9 gives us 8080-8082
+var PORTTWO = 7050;
 
 function handleRequest(request, response) {
-    response.end("Hello World!");
+    response.end("Hello World! from 7000");
+}
+
+function handleRequestTwo(request, response) {
+    response.end("Hello World! from 7050");
 }
 
 var server = http.createServer(handleRequest);
+var serverTwo = http.createServer(handleRequestTwo);
+
 
 server.listen(PORT, function() {
     console.log("Server listening on " + PORT);
+});
+
+serverTwo.listen(PORTTWO, function() {
+    console.log("Server listening on " + PORTTWO);
 });
